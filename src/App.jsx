683a43c21adfec8dc15e2a79dd6ae6d7b41a9657
Manishavijay06 <dmanishavijay@gmail.com>
2025-08-07 15:44:33 +0530

    just started-day

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
-import Night from './components/Night'
+import './App.css';
+import Day from './components/Day';
+// import Night from './components/Night';
 function App() {
 
   return (
     <>
-      <Night />
+      <Day />
     </>
   )
 }
