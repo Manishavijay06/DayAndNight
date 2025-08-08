09f6ba551eb43fd20ef7f76810973aa9fc84bde4
Manishavijay06 <dmanishavijay@gmail.com>
2025-08-08 11:37:00 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
 import './App.css';
 import Day from './components/Day';
-// import Night from './components/Night';
-function App() {
+import React, { useRef } from "react";
+import gsap from "gsap";
+import Night from './components/Night';
 
+function App() {
+  const containerRef = useRef();
+
+  const scrollToNight = () => {
+    console.log("hi night");
+    gsap.to(containerRef.current, {
+      y: "0vh", // Scroll up to night
+      duration: 1,
+      ease: "power2.inOut",
+    
+    });
+  };
+
+  const scrollToDay = () => {
+    console.log('hi day');
+      gsap.to(containerRef.current, {
+      y: "-100vh", // Scroll down to day
+      duration: 1,
+      ease: "power2.inOut",
+    });
+  };
   return (
     <>
-      <Day />
+    <div style={{ overflow: "hidden", height: "100vh" }}>
+      <div ref={containerRef} style={{ position: "relative", height: "200vh" }}>
+        <div style={{ height: "100vh" }}>
+          <Night onOwlClick={scrollToDay} />
+        </div>
+        <div style={{ height: "100vh" }}>
+          <Day onBirdClick={scrollToNight} />
+        </div>
+      </div>
+    </div>
     </>
   )
 }
--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -7,24 +7,67 @@ import sun from "../assets/sun.png";
 import bird from "../assets/bird.png";
 import branch from "../assets/dayBranch.png";
 
-gsap.registerPlugin(gsap,ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
-export default function Day(){
-    
+export default function Day({onBirdClick}){
+    const sundivRef=useRef(null);
+    const birdRef=useRef(null);
+
+    useGSAP(()=>{
+        gsap.fromTo(sundivRef.current,{
+            x:"-800px",
+            scale:"1",
+        },{
+            x:"-1250px",
+            scale:"1.5",
+            duration:10,
+            ease:"power2.out",
+            ScrollTrigger:{
+                trigger:sundivRef.current,
+                start:"top 60%",
+                end:"top 40%",
+                scrub:true,
+                markers:true,
+            },
+        })
+    }) ;
+
+    const handleClick=()=>{
+        gsap.to(sundivRef.current, {
+            rotation: "+=360",
+            duration: 5,
+            ease:"power1",
+          });
+    };
+
+    const handleMouseEnter=()=>{
+        gsap.fromTo(
+            birdRef.current,
+            { y: 0 },
+            {
+              y: -700,
+              duration: 0.4,
+              ease: "bounce.out",
+              yoyo: true,
+              repeat: 1,
+            }
+          );      
+    };
+
     return (
     <>
     <div className="day-container">
         <div className="sunAndBirds">
-            <div className="sundiv" >
+            <div className="sundiv" ref={sundivRef} onMouseEnter={handleClick}>
                 <img className="sunimg" src={sun} alt="sun"></img>
             </div>
         </div>
         <div className="branchesAndBirds">
             <div className="birdsdiv">
-                <img className="birdimg" src={bird} alt="bird"></img>
+                <img className="birdimg" ref={birdRef} src={bird} alt="bird" onMouseEnter={handleMouseEnter} onClick={onBirdClick}></img>
             </div>
             <div className="branchcdiv">
-                <img className="branchimg" src={branch} alt="branch"></img>
+                <img className="branchimg" src={branch} alt="branch" ></img>
             </div>
 
         </div>
